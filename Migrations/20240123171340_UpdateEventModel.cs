ccd66c80d6cdc7b766354f8aaf8392cff03303ec
ogierlicz <oskargierlicz@gmail.com>
2024-01-23 18:41:07 +0100

    Start + End of event

---
+++ Migrations/20240123171340_UpdateEventModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharedCalendar.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEventModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Events",
+                newName: "Start");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "End",
+                table: "Events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "End",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "Start",
+                table: "Events",
+                newName: "Date");
+        }
+    }
+}
--- Models/Event.cs
+++ Models/Event.cs
@@ -4,7 +4,8 @@ namespace SharedCalendar.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End  { get; set; }
         public string Color { get; set; }
     }
 }
